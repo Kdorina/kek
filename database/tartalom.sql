b237fac061740515e5005d4dad130ff629d4aca3
Kovács Dorina <kovacsdorina2@gmail.com>
2022-04-28 08:50:01 +0200

    Adatbázishoz tartalom hozzáadva

---
+++ database/tartalom.sql
@@ -0,0 +1,9 @@
+insert into dolgozok
+-- milyen mezőket adunk meg
+(nev, telepules, fizetes)
+values
+('Tangó Béla', 'Szeged', 385),
+('Okos Árpád', 'Szeged', 375),
+('Lehet Károly', 'Szeged', 342),
+('Terem Irén', 'Szeged', 368),
+('Fakó Lajos', 'Szeged', 384);
