413cc7b1334775505006a5872cfbfe772fbe85a7
Kovács Dorina <kovacsdorina2@gmail.com>
2022-04-28 09:14:39 +0200

    Lekérés működik

--- src/index.php
+++ src/index.php
@@ -6,5 +6,19 @@ if($con -> connect_error){
     die('Hiba! Sikertelen kapcsolódás!');
 }
 
-echo "ok";
+// echo "ok";
+
+$sql = <<< EOT
+select*from dolgozok
+
+EOT;
+if($result = $con-> query($sql)){
+    echo "Lekérés ok";
+}else{
+    echo "Hiba! Lekérés sikertelen";
+}
+while($row = $result->fetch_assoc()){
+    echo $row['nev'];
+}
+
 $con -> close();
